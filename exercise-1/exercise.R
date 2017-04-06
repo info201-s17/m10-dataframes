3a4caa5705a57e68d2996f6184e4f8540ee6fb60
Richard Jiang <Richard Jiang>
2017-04-06 15:49:08 -0700

    Completed Exeercise 1 & 2

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,20 +2,23 @@
 
 # Create a vector of the number of points the Seahawks scored the first 4 games of last season
 # Hint: (google "Seahawks scores 2016")
+points <- c(17, 11, 27, 23)
 
 # Create a vector of the number of points the Seahawks have allowed to be scored against them in the first 4 games
-
+points.against <- c(16, 18, 17, 21)
 
 # Combine your two vectors into a dataframe
-
+seahawks.points <- data.frame(scored = points, allowed = points.against)
 
 # Create a new column "diff" that is the difference in points
-
+seahawks.points$diff <- seahawks.points$scored - seahawks.points$allowed
 
 # Create a new column "won" which is TRUE if the Seahawks wom
-
+seahawks.points$won <- seahawks.points$diff > 0
 
 # Create a vector of the opponents
-
+opponents <- c("Chiefs", "Vikings", "Cowboys", "Raiders")
 
 # Assign your dataframe rownames of their opponents
+rownames(seahawks.points) <- opponents
+
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,51 +1,54 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100)
-
+employees <- c(paste("Employee ", 1:100))
 
 # Create a vector of 2014 salaries using the runif function
-
+old.salaries <- runif(100, 10000, 20000)
 
 # Create a vector of 2015 salaries that are typically higher than the 2014 salaires (use runif again)
-
+new.salaries <- runif(100, 15000, 25000)
 
 # Create a data.frame 'salaries' by combining the vectors you just made
-
+salaries <- data.frame(old.salaries, new.salaries)
+row.names(salaries) <- employees
 
 # Create a column 'raise' that stores the size of the raise between 2014 and 2015
-
+salaries$raise <- new.salaries - old.salaries
 
 # Create a column 'got.raise' that is TRUE if the person got a raise
-
+salaries$got.raise <- salaries$raise > 0
 
 # Retrieve values from your data frame to answer the following questions:
 
 # What was the 2015 salary of employee 57
-
+salaries$new.salaries[[57]]
 
 # How many employees got a raise?
-
+table(salaries$got.raise)["TRUE"]
 
 # What was the value of the highest raise?
-
+salaries$raise[which.max(salaries$raise)]
 
 # What was the name of the employee who recieved the highest raise?
-
+row.names(salaries)[which.max(salaries$raise)]
 
 # What was the largest decrease in salaries between the two years?
-
+salaries$raise[which.min(salaries$raise)]
 
 # What was the name of the employee who recieved largest decrease in salary?
-
+row.names(salaries)[which.min(salaries$raise)]
 
 # What was the average salary increase?
-
+mean(salaries$raise)
 
 ### Bonus ###
 
 # Write a .csv file of your salaries to your working directory
-
+write.csv(salaries, file = "Salary Table.csv", )
 
 # For people who did not get a raise, how much money did they lose?
-
+loss <- !salaries$got.raise
+loss.salaries <- salaries$raise[loss]
+mean(loss.salaries)
 
 # Is that what you expected them to lose?
 
